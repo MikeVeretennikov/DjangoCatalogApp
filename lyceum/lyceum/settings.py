d81357c6385aa5c180d47b79d824f03f7f4774f9
Mike Veretennikov <Mishaveret@yandex.ru>
2024-10-01 23:09:57 +0300

    formatting

--- lyceum/lyceum/settings.py
+++ lyceum/lyceum/settings.py
@@ -9,6 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
+
 import os
 from pathlib import Path
 
@@ -28,9 +29,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv("DJANGO_SECRET_KEY", default="aboba")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv("DJANGO_DEBUG", default=False)
+DEBUG = bool(os.getenv("DJANGO_DEBUG", default=False))
 
-print(SECRET_KEY, DEBUG)
 
 ALLOWED_HOSTS = []
 
