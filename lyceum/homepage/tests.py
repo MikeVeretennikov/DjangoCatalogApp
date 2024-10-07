09c58a094d7a60009edce6be4587662c93f4af21
Mike Veretennikov <Mishaveret@yandex.ru>
2024-10-07 19:22:02 +0300

    add /coffee/ endpoint and tests

--- lyceum/homepage/tests.py
+++ lyceum/homepage/tests.py
@@ -5,3 +5,11 @@ class StaticURLTests(TestCase):
     def test_homepage_endpoint_correct(self):
         response = Client().get("/")
         self.assertEqual(response.status_code, 200)
+
+    def test_coffee_endpoint_correct_text(self):
+        response = Client().get("/coffee/")
+        self.assertIn("Я чайник", response.content.decode())
+
+    def test_coffee_endpoint_correct_status_code(self):
+        response = Client().get("/coffee/")
+        self.assertEqual(response.status_code, 418)
