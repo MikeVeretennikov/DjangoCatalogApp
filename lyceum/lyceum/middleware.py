7bbd031c8f83d4af2d279fed30e0c751700e7427
Mike Veretennikov <Mishaveret@yandex.ru>
2024-10-10 19:41:16 +0300

    update middleware

--- lyceum/lyceum/middleware.py
+++ lyceum/lyceum/middleware.py
@@ -3,6 +3,11 @@ import re
 from django.conf import settings
 
 
+@staticmethod
+def reverse_russian_words(content):
+    return re.sub(r"[А-яёЁ]+", lambda m: m.group()[::-1], content)
+
+
 class ReverseResponseMiddleware:
     count = 0
 
@@ -12,31 +17,15 @@ class ReverseResponseMiddleware:
     def __call__(self, request):
         response = self.get_response(request)
 
-        if settings.ALLOW_REVERSE:
-            ReverseResponseMiddleware.count += 1
+        if not settings.ALLOW_REVERSE:
+            return response
 
-        if settings.ALLOW_REVERSE and ReverseResponseMiddleware.count >= 10:
+        ReverseResponseMiddleware.count += 1
+        if ReverseResponseMiddleware.count >= 10:
+            ReverseResponseMiddleware.count = 0
             content = response.content.decode()
             new_content = reverse_russian_words(content)
 
             response.content = new_content.encode("utf-8")
 
-            ReverseResponseMiddleware.count = 0
-
         return response
-
-
-def reverse_russian_words(sentence):
-
-    def reverse_word(word):
-        return word[::-1]
-
-    russian_words_pattern = r"\b[а-яА-ЯёЁ]+\b"
-
-    reversed_sentence = re.sub(
-        russian_words_pattern,
-        lambda match: reverse_word(match.group()),
-        sentence,
-    )
-
-    return reversed_sentence
