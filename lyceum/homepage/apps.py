c47b4966329ea514e38ddf4ea3c56a81c85d4d6a
Mike Veretennikov <Mishaveret@yandex.ru>
2024-10-11 22:59:29 +0300

    style: format accroding flake8

--- lyceum/homepage/apps.py
+++ lyceum/homepage/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class HomepageConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
-    name = "homepage" 
+    name = "homepage"
     verbose_name = "Главная"
