d09acb6febfd85651ff4d60fe24971db13ff71d3
Mike Veretennikov <Mishaveret@yandex.ru>
2024-10-30 16:52:30 +0300

    style: formatting

--- lyceum/catalog/models.py
+++ lyceum/catalog/models.py
@@ -61,8 +61,9 @@ class Tag(core.models.AbstractModel):
     normalized_name = django.db.models.CharField(
         max_length=150,
         unique=False,
-        null=True,
+        blank=True,
         default=None,
+        null=True,
     )
 
     class Meta:
@@ -101,6 +102,7 @@ class Category(core.models.AbstractModel):
     normalized_name = django.db.models.CharField(
         max_length=150,
         unique=False,
+        blank=True,
         default=None,
         null=True,
     )
@@ -121,32 +123,6 @@ class Category(core.models.AbstractModel):
             raise django.core.exceptions.ValidationError(
                 "Категория с похожим названием уже существует",
             )
-
-
-class OnMainManager(django.db.models.Manager):
-    def get_queryset(self):
-        return (
-            super()
-            .get_queryset()
-            .filter(is_on_main=True, category__is_published=True, is_published=True)
-            .select_related("category", "main_image")
-            .prefetch_related("tags")
-            .only("name", "category", "text")
-            .order_by("name")
-        )
-    
-
-class PublishedManager(django.db.models.Manager):
-    def get_queryset(self):
-        return (
-            super()
-            .get_queryset()
-            .filter(category__is_published=True, is_published=True)
-            .select_related("category", "main_image")
-            .prefetch_related("tags")
-            .only("name", "category", "text")
-            .order_by("category__name", "name")
-        )
 
 
 class Item(core.models.AbstractModel):
@@ -174,10 +150,6 @@ class Item(core.models.AbstractModel):
         default=False,
         help_text="введите принадлежит ли к главной странице товар",
     )
-
-    objects = django.db.models.Manager()
-    on_main = OnMainManager()
-    published = PublishedManager()
 
     def image_tmb(self):
         if self.main_image:
